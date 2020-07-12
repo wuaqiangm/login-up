420eee01e2f0d67b708435c8550ca19184c6db38
manager <504518146@qq.com>
2020-07-12 16:38:05 +0800

    tag commit try agin!

--- login.py
+++ login.py
@@ -3,3 +3,7 @@ this is smart dog i also very love it my animals!
 <<<<<<< HEAD
 hello everybody!
 git config --global credential.helper store!
+this is a order for git hub!
+git tag -a v1.0 -m 'git hub tag you know!'
+git push origin v1.0
+over
